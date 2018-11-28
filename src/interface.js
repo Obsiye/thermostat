f820e5e3ebb3dfc28b20f55fff11d5ca641fee24
Obsiye <mrabdirizak@gmail.com>
2018-11-28 15:25:02 +0000

    Api request to open weather api

--- src/interface.js
+++ src/interface.js
@@ -34,4 +34,30 @@ $(document).ready(function() {
     $('#temperature').attr('class', thermostat.energyUsage());
   };
 
+
+  $.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather",
+    type: "GET",
+    dataType: "JSON",
+    data: {
+      id: "2643743",
+      units: "metric",
+      APPID: "81e2f5088f4d06b3466d354016c026a9"
+    },
+    success: function (data) {
+      alert(data.main.temp);
+    },
+    error: function (data, textStatus, errorThrown) {
+      //Do Something to handle error
+      alert(textStatus);
+    }
+  });
+
+  // london id 2643743
+
+  // api.openweathermap.org server name
+
+  // Api key 81e2f5088f4d06b3466d354016c026a9
+
+  
 });
